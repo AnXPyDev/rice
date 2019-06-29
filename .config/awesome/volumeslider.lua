c2580f25524872a5824672a8fef1d762054d30d0
AnXPyDev <jkism332@gmail.com>
2019-06-29 14:21:58 +0000

    Working on slider class

--- .config/awesome/volumeslider.lua
+++ .config/awesome/volumeslider.lua
@@ -1,5 +1,14 @@
-volumeslider = {}
+Slider = {}
 
-function volumeslider:setup()
-  
+function Slider:new()
+  local ins = {}
+  setmetatable(ins, self)
+  self.__index = self
+  return ins
+end
+
+function Slider:setup(args)
+  self.screen = args.screen
+  self.wibox = {}
+  self.sliders = {}
 end
